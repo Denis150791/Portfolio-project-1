36cd85306a778fae897fc1b34f5269d5183cc7e7
Den4ik <3532817@gmail.com>
2022-06-11 19:06:47 +0300

    Second commit

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -1,7 +1,18 @@
 let element = document.querySelector('h2');
 
 let keyPlay = function(event) {
-    event.target.style.backgroundColor = 'green';
+    event.target.style.border = '2px solid green';
+
   }
 
+  let keyReturn = function(event) {
+    event.target.style.border = '';
+
+  }
+
+
 element.onmouseenter = keyPlay;
+element.onmouseout = keyReturn;
+
+
+
